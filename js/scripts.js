6fc20ae987af85ac373c3f6aa9f6e2e9b8d2c496
Kate Hall <kwarrenhall@gmail.com>
2016-01-17 12:40:34 -0800

    edited code to ensure correnct folder and file names, all lines of code are closed.

--- js/scripts.js
+++ js/scripts.js
@@ -10,19 +10,20 @@
         // $(".results").children("div").hide();
         $(".results").hide();
 
-        if ((budget === "less") && (sunsnow === "sun") && (crowds === "peopleloving") {
-          $(".tulum").show();
-        } else if ((budget === "less") && (sunsnow === "sun") && (crowds === "xenophobic") { $(".oasis").show();
-        } else if ((budget === "more") && (sunsnow === "sun")) && (crowds === "peopleloving"){
-          $(".burningman").show();
-        } else if ((budget === "less") && (nature === "superhero")) && (dependents === "children"){
-          $(".comicon").show();
-        } else if ((budget === "more") && (sunsnow === "snow")) && crowds === "xenophobic"){
+        if ((budget === "less") && (sunsnow === "sun") && (crowds === "peopleloving")) {
+         $(".tulum").show();
+        } else if ((budget === "less") && (sunsnow === "sun") && (crowds === "xenophobic")) {
+         $(".oasis").show();
+        } else if ((budget === "more") && (sunsnow === "sun") && (crowds === "peopleloving")) {         $(".burningman").show();
+        } else if ((budget === "less") && (nature === "superhero") && (dependents === "children")) {
+         $(".comicon").show();
+        } else if ((budget === "more") && (sunsnow === "snow") && crowds === "xenophobic")) {
           $(".kremlin").show();
         } else if ((budget === "more") && (nature === "superhero")(dependents === "animals")) {
           $(".safari").show();
-        } else if ((nature === "poet") && (sunsnow === "xenophobic")) && (dependants === animals) {
-            $(".animalshelter").show();
+        } else if ((nature === "poet") && (sunsnow === "xenophobic") && (dependants === "animals")) {
+         $(".animalshelter").show();
+            
 
 
       event.preventDefault();
