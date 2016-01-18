ad5f72101a02b79ad9ace6fbbee48eb4a417fca3
Kate Hall <kwarrenhall@gmail.com>
2016-01-17 21:11:47 -0800

    add files problem with if statement in scripts code

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 
+// source of code more branching
     $(document).ready(function() {
       $("form#vacation").submit(function(event) {
         var budget = $("select#budget").val();
@@ -10,21 +11,34 @@
         // $(".results").children("div").hide();
         $(".results").hide();
 
-        if ((budget === "less") && (sunsnow === "sun") && (crowds === "people") && (nature === "poet") && (dependents === "children")) {
-         $(".tulum").show();
-         $(".tulum").show();
-         $(".tulum").show();
-        } else if ((budget === "less") && (sunsnow === "sun") && (crowds === "xenophobic")) {
-         $(".oasis").show();
-        } else if ((budget === "more") && (sunsnow === "sun") && (crowds === "people")) {         $(".burningman").show();
-        } else if ((budget === "less") && (nature === "superhero") && (dependents === "children")) {
-         $(".comicon").show();
-        } else if ((budget === "more") && (sunsnow === "snow") && crowds === "xenophobic")) {
-          $(".kremlin").show();
-        } else if ((budget === "more") && (nature === "superhero")(dependents === "animals")) {
-          $(".safari").show();
-        } else if ((nature === "poet") && (sunsnow === "xenophobic") && (dependants === "animals")) {
-         $(".animalshelter").show();
+
+
+        if ((budget === "lots") && (sunsnow === "sun") && (crowds === "xenophobic") && (nature === "poet") && (dependents === "animals")) {
+         $(".safari").show();
+         $(".safari").show();
+         $(".safari").show();
+        }
+        else {
+        $(".oasis").show();}
+
+
+
+        // if ((budget === "lots") && (sunsnow === "sun") && (crowds === "xenophobic") && (nature === "poet") && (dependents === "dependents")) {
+        // $(".safari").show();
+        // }
+
+        //
+        // else if ((budget === "less") && (sunsnow === "sun") && (crowds === "xenophobic")) {
+        //  $(".oasis").show();
+        // } else if ((budget === "more") && (sunsnow === "sun") && (crowds === "people")) {         $(".burningman").show();
+        // } else if ((budget === "less") && (nature === "superhero") && (dependents === "children")) {
+        //  $(".comicon").show();
+        // } else if ((budget === "more") && (sunsnow === "snow") && crowds === "xenophobic")) {
+        //   $(".kremlin").show();
+        // } else if ((budget === "more") && (nature === "superhero")(dependents === "animals")) {
+        //   $(".safari").show();
+        // } else if ((nature === "poet") && (sunsnow === "xenophobic") && (dependants === "animals")) {
+        //  $(".animalshelter").show();
 
 
 
