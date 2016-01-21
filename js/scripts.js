9f5e687e244cb6d89ca0279894d044658062579a
Kate Hall <kwarrenhall@gmail.com>
2016-01-20 23:36:41 -0800

    add all changes to index, scripts, css post merge

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 
-// source of code more branching
     $(document).ready(function() {
       $("form#vacation").submit(function(event) {
         var budget = $("select#budget").val();
@@ -8,43 +7,37 @@
         var nature = $("select#nature").val();
         var dependents = $("select#dependents").val();
 
-        // $(".results").children("div").hide();
+    
         $(".results").hide();
 
-if (sunsnow === "sun"){
-  $(".safari").show();
-};
 
-        // if ((budget === "lots") && (sunsnow === "sun") && (crowds === "xenophobic") && (nature === "poet") && (dependents === "animals")) {
-        //  $(".safari").show();
-        // //  $(".safari").show();
-        // //  $(".safari").show();
-        // }
-      //   else {
-      //   $(".oasis").show();
-      // }
-
-
-
-        // if ((budget === "lots") && (sunsnow === "sun") && (crowds === "xenophobic") && (nature === "poet") && (dependents === "dependents")) {
-        // $(".safari").show();
-        // }
-
-        //
-        // else if ((budget === "less") && (sunsnow === "sun") && (crowds === "xenophobic")) {
-        //  $(".oasis").show();
-        // } else if ((budget === "more") && (sunsnow === "sun") && (crowds === "people")) {         $(".burningman").show();
-        // } else if ((budget === "less") && (nature === "superhero") && (dependents === "children")) {
-        //  $(".comicon").show();
-        // } else if ((budget === "more") && (sunsnow === "snow") && crowds === "xenophobic")) {
-        //   $(".kremlin").show();
-        // } else if ((budget === "more") && (nature === "superhero")(dependents === "animals")) {
-        //   $(".safari").show();
-        // } else if ((nature === "poet") && (sunsnow === "xenophobic") && (dependants === "animals")) {
-        //  $(".animalshelter").show();
-
+// start of lots--
+        if ((budget === "lots") && (sunsnow === "sun") && (crowds === "xenophobic") && (nature === "poet") && (dependents === "animal")) {
+         $(".animalshelter").show();
+       } else if ((budget === "lots") && (sunsnow === "sun") &&  (crowds === "xenophobic") && (nature === "poet") && (dependents === "children")) {
+          $(".burningman").show();
+        } else if ((budget === "lots") && (sunsnow === "sun") &&  (crowds === "xenophobic") && (nature === "superhero") && (dependents === "animal")) {
+           $(".comicon").show();
+         } else if ((budget === "lots") && (sunsnow === "sun") &&  (crowds === "people") && (nature === "poet") && (dependents === "animal")) {
+            $(".oasis").show();
+          } else if ((budget === "lots") && (sunsnow === "snow") &&  (crowds === "xenophobic") && (nature === "poet") && (dependents === "animal")) {
+             $(".safari").show();
+// start of less-
+           } else if ((budget === "less") && (sunsnow === "sun") && (crowds === "xenophobic") && (nature === "poet") && (dependents === "animal")) {
+              $(".safari").show();
+            } else if ((budget === "less") && (sunsnow === "sun") &&  (crowds === "xenophobic") && (nature === "poet") && (dependents === "children")) {
+               $(".burningman").show();
+             } else if ((budget === "less") && (sunsnow === "sun") &&  (crowds === "xenophobic") && (nature === "superhero") && (dependents === "animal")) {
+                $(".comicon").show();
+              } else if ((budget === "less") && (sunsnow === "sun") &&  (crowds === "people") && (nature === "poet") && (dependents === "animal")) {
+                 $(".oasis").show();
+               } else if ((budget === "less") && (sunsnow === "snow") &&  (crowds === "xenophobic") && (nature === "poet") && (dependents === "animal")) {
+                  $(".safari").show();
+         } else {
+           $(".safari").show();
+         }
 
 
       event.preventDefault();
-      });
-    });
+     });
+ });
